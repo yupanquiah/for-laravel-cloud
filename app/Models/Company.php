525d5bd1338cebc872dbdfd3c64d8a38331acb08
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2025-05-04 10:58:35 -0500

    feat: add fillable attributes to Company model for mass assignment

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,6 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
+    protected $fillable = [
+        'country',
+        'company_name',
+        'type_company',
+        'nit',
+        'phone',
+        'mail',
+        'tax_amount',
+        'tax_name',
+        'currency',
+        'address',
+        'city',
+        'department',
+        'postal_code',
+        'logo',
+    ];
+
     public function users(){
       return $this->hasMany(User::class);
     }
