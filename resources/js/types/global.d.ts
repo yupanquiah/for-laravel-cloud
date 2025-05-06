39f660132c1ac8d9798195f9c10efb2d1c75fe80
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2025-05-06 16:17:01 -0500

    fix: update global type definitions for canvas-confetti module

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,5 +1,10 @@
-import type { route as routeFn } from 'ziggy-js';
+import type { route as routeFn } from 'ziggy-js'
 
 declare global {
   const route: typeof routeFn;
+}
+
+declare module 'canvas-confetti' {
+  const confetti: any;
+  export default confetti;
 }
