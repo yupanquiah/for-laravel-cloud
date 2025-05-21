e30b0db5ac87bb5079d13015cf61a1f8afa5b97b
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2025-05-21 17:14:53 -0500

    style: refactor CreateRole component structure for improved readability

--- resources/js/pages/admin/roles/create.tsx
+++ resources/js/pages/admin/roles/create.tsx
@@ -22,43 +22,41 @@ export const CreateRole = () => {
   };
 
   return (
-    <>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button>
-            <Plus />
-            Agregar
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus />
+          Agregar
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader className="mb-4">
+          <DialogTitle>Crea un nuevo rol</DialogTitle>
+          <DialogDescription>Agrega un nuevo rol aquí. Guarda los cambios cuando termines.</DialogDescription>
+        </DialogHeader>
+        <form id="create-role" onSubmit={submit}>
+          <Label className="flex flex-col items-start gap-4">
+            Nombre de rol
+            <Input type="text" required name="name" value={data.name} onChange={(e) => setData('name', e.target.value)} />
+            <InputError message={errors.name} />
+          </Label>
+        </form>
+        <DialogFooter className="flex items-center justify-center gap-4">
+          <Transition
+            show={recentlySuccessful ? confetti() : ''}
+            enter="transition ease-in-out"
+            enterFrom="opacity-0"
+            leave="transition ease-in-out"
+            leaveTo="opacity-0"
+          >
+            <p className="text-sm text-neutral-600">Guardado</p>
+          </Transition>
+          <Button form="create-role" type="submit" disabled={processing}>
+            {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
+            Crear rol
           </Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader className="mb-4">
-            <DialogTitle>Crea un nuevo rol</DialogTitle>
-            <DialogDescription>Agrega un nuevo rol aquí. Guarda los cambios cuando termines.</DialogDescription>
-          </DialogHeader>
-          <form id="create-role" onSubmit={submit}>
-            <Label className="flex flex-col items-start gap-4">
-              Nombre de rol
-              <Input type="text" required name="name" value={data.name} onChange={(e) => setData('name', e.target.value)} />
-              <InputError message={errors.name} />
-            </Label>
-          </form>
-          <DialogFooter className="flex items-center justify-center gap-4">
-            <Transition
-              show={recentlySuccessful ? confetti() : ''}
-              enter="transition ease-in-out"
-              enterFrom="opacity-0"
-              leave="transition ease-in-out"
-              leaveTo="opacity-0"
-            >
-              <p className="text-sm text-neutral-600">Guardado</p>
-            </Transition>
-            <Button form="create-role" type="submit" disabled={processing}>
-              {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
-              Crear rol
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
