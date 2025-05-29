eb7e7ece3b81dde86f5e1aac0ba0a5c1dfcf133d
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2025-05-29 15:00:00 -0500

    feat: add total roles display to Dashboard component for enhanced user information

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,24 +1,34 @@
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
-import AppLayout from '@/layouts/app-layout';
-import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/react';
+import { Card } from '@/components/ui/card'
+import { PlaceholderPattern } from '@/components/ui/placeholder-pattern'
+import AppLayout from '@/layouts/app-layout'
+import { type BreadcrumbItem } from '@/types'
+import { Head } from '@inertiajs/react'
 
 const breadcrumbs: BreadcrumbItem[] = [
   {
     title: 'Dashboard',
     href: '/dashboard',
   },
-];
+]
 
-export default function Dashboard() {
+interface DashboardProps {
+  total_roles: number
+}
+
+
+export default function Dashboard ({ total_roles }: DashboardProps) {
   return (
+
+
     <AppLayout breadcrumbs={breadcrumbs}>
       <Head title="Dashboard" />
       <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-          </div>
+
+          <Card className='p-4 aspect-video'>
+            Total de roles{' '}
+            {total_roles}
+          </Card>
           <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
             <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
           </div>
@@ -26,10 +36,8 @@ export default function Dashboard() {
             <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
           </div>
         </div>
-        <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
-          <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-        </div>
+
       </div>
     </AppLayout>
-  );
+  )
 }
