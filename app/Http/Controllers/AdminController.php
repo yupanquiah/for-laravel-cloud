21e39b1dbcaf049a677db64f11f728772c7324f0
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2025-05-29 15:00:00 -0500

    feat: include total roles in Admin and Dashboard components for enhanced user information

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,16 +5,30 @@ namespace App\Http\Controllers;
 use Inertia\Inertia;
 use App\Models\Company;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
   public function index() {
+    $total_roles = DB::table('roles')->count();
     $company = Auth::user()->company;
     $company_name = $company->company_name;
 
     return Inertia::render('admin/index', [
-      'company_name' => $company_name
+      'company_name' => $company_name,
+      'total_roles' => $total_roles,
+    ]);
+  }
+
+  public function dashboard() {
+    $total_roles = DB::table('roles')->count();
+    $company = Auth::user()->company;
+    $company_name = $company->company_name;
+
+    return Inertia::render('dashboard', [
+      'company_name' => $company_name,
+      'total_roles' => $total_roles,
     ]);
   }
 }
