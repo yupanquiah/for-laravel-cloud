3bea490129ac3e9d636737dd65631b5a96746646
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2025-05-29 15:00:00 -0500

    style: format code for consistency in Admin and Dashboard components

--- resources/js/pages/admin/index.tsx
+++ resources/js/pages/admin/index.tsx
@@ -1,18 +1,18 @@
-import { Card } from '@/components/ui/card'
-import AppLayout from '@/layouts/app-layout'
-import { BreadcrumbItem } from '@/types'
-import { Head } from '@inertiajs/react'
+import { Card } from '@/components/ui/card';
+import AppLayout from '@/layouts/app-layout';
+import { BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
 
 const breadcrumbs: BreadcrumbItem[] = [
   {
     title: 'Administración',
     href: '/admin',
   },
-]
+];
 
 interface Admin {
-  company_name: string
-  total_roles: number
+  company_name: string;
+  total_roles: number;
 }
 
 const Admin = ({ company_name, total_roles }: Admin) => {
@@ -22,14 +22,11 @@ const Admin = ({ company_name, total_roles }: Admin) => {
       <main className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
         <h3 className="text-2xl font-semibold tracking-tight">Bienvenido {company_name}</h3>
         <section>
-          <Card className='p-4'>
-            Total de roles{' '}
-            {total_roles}
-          </Card>
+          <Card className="p-4">Total de roles {total_roles}</Card>
         </section>
       </main>
     </AppLayout>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,34 +1,27 @@
-import { Card } from '@/components/ui/card'
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern'
-import AppLayout from '@/layouts/app-layout'
-import { type BreadcrumbItem } from '@/types'
-import { Head } from '@inertiajs/react'
+import { Card } from '@/components/ui/card';
+import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
 
 const breadcrumbs: BreadcrumbItem[] = [
   {
     title: 'Dashboard',
     href: '/dashboard',
   },
-]
+];
 
 interface DashboardProps {
-  total_roles: number
+  total_roles: number;
 }
 
-
-export default function Dashboard ({ total_roles }: DashboardProps) {
+export default function Dashboard({ total_roles }: DashboardProps) {
   return (
-
-
     <AppLayout breadcrumbs={breadcrumbs}>
       <Head title="Dashboard" />
       <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-
-          <Card className='p-4 aspect-video'>
-            Total de roles{' '}
-            {total_roles}
-          </Card>
+          <Card className="aspect-video p-4">Total de roles {total_roles}</Card>
           <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
             <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
           </div>
@@ -36,8 +29,7 @@ export default function Dashboard ({ total_roles }: DashboardProps) {
             <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
           </div>
         </div>
-
       </div>
     </AppLayout>
-  )
+  );
 }
